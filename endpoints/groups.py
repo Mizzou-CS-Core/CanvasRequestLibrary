dbb07e8d19eb96e622b80ca656ad4a2d97c14165
Matt Marlow <thequantumblink@gmail.com>
2025-04-24 01:40:57 -0500

    fix: moved groups outside of {}

--- endpoints/groups.py
+++ endpoints/groups.py
@@ -2,5 +2,5 @@ class GroupService:
     def __init__(self, api_client) -> None:
         self._api_client = api_client
     def get_groups_from_course(self, course_id: int):
-        endpoint: str = f"courses/{str(course_id)/groups}"
+        endpoint: str = f"courses/{str(course_id)}/groups"
         return self._api_client.request("GET", endpoint)
