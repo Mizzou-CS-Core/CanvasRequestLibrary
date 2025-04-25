9e3b7fac2fe74bcd9dfb1af36aed9731d179a340
Matt Marlow <thequantumblink@gmail.com>
2025-04-25 13:41:42 -0500

    fix: forgot to add Assignment to setup

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-from .client          import CanvasClient
-from .models.group    import Group
-from .models.person   import Person
+from .client import CanvasClient
+from .models.group import Group
+from .models.person import Person
+from .models.assignment import Assignment
 
 __all__ = [
     "CanvasClient",
     "Group",
     "Person"
+    "Assignment"
 ]
