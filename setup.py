9b2fa225b8495e63c4f6d11da12ea61ac8d9f246
Matt Marlow <thequantumblink@gmail.com>
2025-04-28 16:18:14 -0500

    feat: adjusted structure to allow for better pip usage

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
   name="canvas_lms_api",
   version="0.3.0",
-  packages=find_packages(),
+  packages=find_packages(include=['canvas_lms_api', 'canvas_lms_api.*']),
+  install_requires=[]
 )
