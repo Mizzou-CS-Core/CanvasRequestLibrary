63e711960ce2aaaa2f91c0831ee57256bba4a896
Matt Marlow <thequantumblink@gmail.com>
2025-05-01 00:24:24 -0500

    fix: syntax

--- canvas_lms_api/endpoints/courses.py
+++ canvas_lms_api/endpoints/courses.py
@@ -12,7 +12,7 @@ class CourseService:
         if return_json:
             return json
         return Course.from_json(json)
-    def get_people_from_course(self, course_id: int, return_json: bool = False, per_page: int=100)
+    def get_people_from_course(self, course_id: int, return_json: bool = False, per_page: int=100):
         options = f"per_page={per_page}"
         endpoint: str = f"courses/{course_id}/search_users?{options}"
         json = self._api_client.request("GET", endpoint)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="canvas_lms_api",
-  version="0.5.1",
+  version="0.5.1.1",
   packages=find_packages(include=['canvas_lms_api', 'canvas_lms_api.*']),
   install_requires=["requests"]
 )
