be06b0fe52fb8da9d8a82c2c24fa0f58596de784
Matt Marlow <thequantumblink@gmail.com>
2025-05-01 14:37:16 -0500

    feat: replaced it with get

--- canvas_lms_api/models/person.py
+++ canvas_lms_api/models/person.py
@@ -9,5 +9,5 @@ class Person:
     def parse_people_from_json(people_json) -> []:
         people = []
         for body in people_json:
-            people.append(Person(name=body['name'], canvas_id=body['id'], login_id=body['login_id'], sortable_name=body['sortable_name'], email=body['email'] or ""))
+            people.append(Person(name=body['name'], canvas_id=body['id'], login_id=body['login_id'], sortable_name=body['sortable_name'], email=body.get("email") or ""))
         return people
